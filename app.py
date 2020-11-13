ac4b58436c8fa5f6cb223425cf83536d94ce8613
sstoneju <sstoneju@github.com>
2020-11-13 14:54:54 +0900

    fix: convert_date

--- app.py
+++ app.py
@@ -40,14 +40,20 @@ def history_tickers(ticker=None):
     today = datetime.now()
     params = app.current_request.query_params if app.current_request.query_params else {}
 
-    start_date = params['start_date'] if 'start_date' in params else today - timedelta(weeks=52)
-    end_date = params['end_date'] if 'end_date' in params else today
+    start_date = convert_string_to_date(str(params['start'])) if 'start' in params else today - timedelta(weeks=52)
+    end_date = convert_string_to_date(params['end']) if 'end' in params else today
 
     downloader =  Downloader()
     histries = downloader.fetchh_etf_dict(ticker=ticker, start_date=start_date, end_date=end_date)
 
     return {'result': 'success', 'list':histries}
 
+def convert_string_to_date(str_date:str, form='%Y%m%d'):
+    str_date = ''.join(str_date.split('.')) if str_date.split('.') else str_date
+    str_date = ''.join(str_date.split('-')) if str_date.split('-') else str_date
+    str_date = ''.join(str_date.split('/')) if str_date.split('/') else str_date
+    date_obj = datetime.strptime(str_date, form)
+    return date_obj
 
 @app.route('/alarm', methods=['GET'])
 def alarm_tickers():
