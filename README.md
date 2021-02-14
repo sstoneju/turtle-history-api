d3c210deeabec26b99cff1d0e3ec4ffa96e0f745
sstoneju <sstoneju@kakao.com>
2021-02-14 23:42:46 +0900

    feat: change what only running with lambda

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Turtle-history-api
-If you’ve deployed your application using `chalice deploy`, 
-you can test the REST API by making requests to the Rest API URL, shown in the output of `chalice deploy`, 
-in our example that would be https://abcd.execute-api.us-west-2.amazonaws.com/api/. 
-If you’re using `chalice local`, you’ll make requests to http://localhost:8000/
+### Basic setting on windows
+```sh
+# python venv create (at mac, windoww both)
+$ python -m venv .venv
+# venv run
+$ .\.venv\Scripts\activate
+# package install
+$ pip install -r requirement.txt
+# package export
+$ pip freeze > requirement.txt
+# run server
+$ flask run
+```
--- app.py
+++ app.py
@@ -1,68 +1,50 @@
 # -*- coding: utf-8 -*-
-from chalice import Chalice
-
+# from chalice import Chalice
+import os
 import sys
 import json
-import logging
-import os
 import time
+from loguru import logger
+from service import history_service
 from datetime import date, datetime, timedelta
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
 
-app = Chalice(app_name='turtle-history-api')
+# app = Chalice(app_name='turtle-history-api')
 
-@app.lambda_function()
-@app.route('/history', methods=['POST'])
-@app.route('/history', methods=['GET'])
-def handler(event=None, context=None):
-    http_param = app.current_request.to_dict()
-    print('event: {}'.format(event))
-    print('http_param: {}'.format(http_param))
-    message = json.dumps(event)
-    context = '{}'.format(context)
-    return {
-        'message' : message,
-        'context' : context
-    }
+def history_handler(request, context):
+    """
+    This function takes in an arbritary financial function and its parameters as inputs and returns the result of that calculation
+    :param request: Dict containing the financial function name (function_name) and its parameters (args)
+    :param context: Lambda execution context
+    :return: Dict with a 'result' entry containing the result of the calculation
+    """
+    logger.info("financial function request: {}".format(request))
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+    try:
+        validate(request, schemas.wrapper_schema)
+    except ValidationError as err:
+        logger.info("Invalid request: {}. Exception: {}".format(request, err))
+        return {'error': err.message}
 
+    function_name = request['function_name']
+    function_handler_name = function_name + "_handler"
+    if hasattr(handlers, function_handler_name):
+        return getattr(handlers, function_handler_name)(request['args'], context)
+    else:
+        return {"error": "Invalid function name: " + function_name + ". Please see documentation for help on supported functions"}
 
-@app.route('/meta')
-def call_meta():
-    return app.current_request.to_dict()
-
-
-@app.route('/history-web/{ticker}')
-def history_tickers(ticker=None):
-    from chalicelib.downloader import Downloader
-
-    '''
-    ticker를 받아서 history를 보여준다.
-    https://aws.github.io/chalice/tutorials/basicrestapi.html 사용법
-
-    start_date=20010505
-    end_date=20201010
-    '''
-
-    if ticker is None:
-        return {'error':'Ticker is None...'}
-
-    today = datetime.now()
-    params = app.current_request.query_params if app.current_request.query_params else {}
-
-    start_date = convert_string_to_date(str(params['start'])) if 'start' in params else today - timedelta(weeks=52)
-    end_date = convert_string_to_date(params['end']) if 'end' in params else today
-
-    downloader =  Downloader()
-    histries = downloader.fetchh_etf_dict(ticker=ticker, start_date=start_date, end_date=end_date)
-
-    return {'result': 'success', 'list':histries}
-
-def convert_string_to_date(str_date:str, form='%Y%m%d'):
-    str_date = ''.join(str_date.split('.')) if str_date.split('.') else str_date
-    str_date = ''.join(str_date.split('-')) if str_date.split('-') else str_date
-    str_date = ''.join(str_date.split('/')) if str_date.split('/') else str_date
-    date_obj = datetime.strptime(str_date, form)
-    return date_obj
+def __validate_arguments(function_name, arguments_json, json_schema):
+    """
+    Validate the arguments in the provided JSON against the provided json schema
+    :param function_name:
+    :param arguments_json:
+    :param json_schema:
+    :return: Dict containing whether the provided json is valid and an error message if validation failed.
+    """
+    try:
+        validate(arguments_json, json_schema)
+        return {'isValid': True}
+    except ValidationError as err:
+        logger.error("Invalid {} request with args: {}. Exception: {}".format(function_name, arguments_json, err))
+        return {'isValid': False, 'error': err.message}
